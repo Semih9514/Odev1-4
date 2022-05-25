175a5a3d2f9cfbfa901b2a7bbfbd98a6d6b388b8
semih.bagdas <semih.bagdas@gmail.com>
2022-05-26 01:54:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Odev1_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        Console.Write("Cumle Giriniz:");
+        string cumle = Console.ReadLine();
+
+        string[] dizi ;
+        dizi = cumle.Split(' ');
+
+        string liste  = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ" + "abcçdefgğhiıjklmnoöprsştuüvyz";
+        int sayac = 0;
+        int t = 0;
+
+        foreach (var kelime in dizi)
+        {
+            Console.WriteLine(kelime);
+            t++;
+        }
+
+        for (int i = 0; i < cumle.Length; i++)
+        {
+            if (liste.Contains(cumle[i]))
+            {
+                sayac++;
+            }
+        }
+        Console.WriteLine("{0} tane  harf vardır.", sayac);
+        Console.WriteLine("{0} tane  kelime vardır.", t);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+<h2>Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.<h2>
